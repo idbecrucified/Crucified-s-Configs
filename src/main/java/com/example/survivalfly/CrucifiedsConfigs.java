9433646c74ae8fd6fbcc33b1f8a17fb807ba0fb9
idbecrucified <idbecrucified@gmail.com>
2026-07-29 16:38:21 -0600

    Update CrucifiedsConfigs.java

--- src/main/java/com/example/survivalfly/CrucifiedsConfigs.java
+++ src/main/java/com/example/survivalfly/CrucifiedsConfigs.java
@@ -1,38 +1,9 @@
 package com.example.survivalfly;
 
 public class CrucifiedsConfigs {
-    // Theme Settings
-    public static String currentTheme = "Gamer";
-
-    // PvP & HUD Elements
     public static boolean toggleSprint = true;
-    public static boolean totemCounter = true;
-    public static boolean armorStatus = true;
     public static boolean cpsDisplay = true;
     public static boolean keystrokes = true;
-    public static boolean fpsCounter = true;
-    
-    // HUD Positions
-    public static int fpsCounterX = 5;
-    public static int fpsCounterY = 5;
-    public static int totemCounterX = 100;
-    public static int totemCounterY = 100;
-    public static int keystrokesX = 5;
-    public static int keystrokesY = 65;
-    public static int cpsDisplayX = 5;
-    public static int cpsDisplayY = 45;
-    public static int armorStatusX = -25;
-    public static int armorStatusY = -50;
-
-    // Performance
-    public static boolean entityCulling = true;
-    public static boolean fpsBooster = false;
-    public static boolean chunkAnimator = false;
-    public static boolean particleMultiplier = false;
-
-    // Graphics
-    public static boolean fullbright = true;
-    public static boolean dynamicLighting = false;
-    public static boolean minimalHud = false;
-    public static boolean weatherChanger = false;
+    public static boolean armorStatus = true;
+    public static boolean totemCounter = true;
 }
