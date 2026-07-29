f6e7d8896bc5d92eda912842947cb2332337bc94
idbecrucified <idbecrucified@gmail.com>
2026-07-29 10:50:09 -0600

    Update CrucifiedsConfigs.java

--- src/main/java/com/example/survivalfly/CrucifiedsConfigs.java
+++ src/main/java/com/example/survivalfly/CrucifiedsConfigs.java
@@ -1,12 +1,19 @@
 package com.example.survivalfly;
 
 public class CrucifiedsConfigs {
-    // PvP
+    // PvP & HUD Elements
     public static boolean toggleSprint = true;
     public static boolean totemCounter = true;
     public static boolean armorStatus = true;
     public static boolean cpsDisplay = true;
     public static boolean keystrokes = true;
+    public static boolean fpsCounter = true;
+    
+    // HUD Positions
+    public static int fpsCounterX = 5;
+    public static int fpsCounterY = 5;
+    public static int totemCounterX = 100;
+    public static int totemCounterY = 100;
 
     // Performance
     public static boolean entityCulling = true;
