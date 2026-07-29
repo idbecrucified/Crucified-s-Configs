f47d5b784f6af8b30456149dd7f69acbca2e952d
idbecrucified <idbecrucified@gmail.com>
2026-07-29 08:54:09 -0600

    Create SurvivalFlyMod.java

---
+++ src/main/java/com/example/survivalfly/SurvivalFlyMod.java
@@ -0,0 +1,10 @@
+package com.example.survivalfly;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SurvivalFlyMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Main server/common initialization (Client-side mod runs mostly in SurvivalFlyClient)
+    }
+}
