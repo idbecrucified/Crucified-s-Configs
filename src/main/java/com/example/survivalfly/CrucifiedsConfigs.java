861b96bc8d0c7249827afba9a2da753d2d751c80
idbecrucified <idbecrucified@gmail.com>
2026-07-29 17:27:58 -0600

    Update CrucifiedsConfigs.java

--- src/main/java/com/example/survivalfly/CrucifiedsConfigs.java
+++ src/main/java/com/example/survivalfly/CrucifiedsConfigs.java
@@ -13,8 +13,8 @@ public class CrucifiedsConfigs {
 
     // HUD Customization Settings
     public static boolean hudBackground = true;
-    public static int hudBackgroundColor = 0x80000000; // Semi-transparent dark
-    public static int hudTextColor = 0xFFFFFFFF;       // White text
+    public static int hudBackgroundColor = 0x80000000; 
+    public static int hudTextColor = 0xFFFFFFFF;       
 
     // Performance Options
     public static boolean fastRender = true;
