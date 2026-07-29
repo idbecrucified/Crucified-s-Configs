4cb40b6d3884675953a09e5871c1010068bdbccf
idbecrucified <idbecrucified@gmail.com>
2026-07-29 10:10:09 -0600

    Update SurvivalFlyClient.java

--- src/main/java/com/example/survivalfly/SurvivalFlyClient.java
+++ src/main/java/com/example/survivalfly/SurvivalFlyClient.java
@@ -5,35 +5,25 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import org.lwjgl.glfw.GLFW;
 
 public class SurvivalFlyClient implements ClientModInitializer {
-    public static KeyBinding configKey;
+    private static KeyBinding openMenuKey;
 
     @Override
     public void onInitializeClient() {
-        configKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.survivalfly.open_menu",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_RIGHT_SHIFT,
-            "category.survivalfly.general"
+        // Register Right Shift keybinding
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.survivalfly.open_menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                "category.survivalfly.general"
         ));
 
-        HudRenderer.register();
-
+        // Listen for key presses during the game tick
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player == null) return;
-
-            if (configKey.wasPressed()) {
-                client.setScreen(new LunarModMenuScreen(null));
-            }
-
-            if (CrucifiedsConfigs.fullbright) {
-                client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 220, 0, false, false, false));
-            } else if (client.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
-                client.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
+            while (openMenuKey.wasPressed()) {
+                client.setScreen(new LunarModMenuScreen());
             }
         });
     }
