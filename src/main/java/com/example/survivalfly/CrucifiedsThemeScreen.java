3c33c10e2d51e13ed5e785b1d84fd26bdace6a08
idbecrucified <idbecrucified@gmail.com>
2026-08-27 16:44:59 -0600

    Update CrucifiedsThemeScreen.java

--- src/main/java/com/example/survivalfly/CrucifiedsThemeScreen.java
+++ src/main/java/com/example/survivalfly/CrucifiedsThemeScreen.java
@@ -17,11 +17,11 @@ public class CrucifiedsThemeScreen extends Screen {
     protected void init() {
         int centerX = this.width / 2;
         int centerY = this.height / 2;
-        int panelHeight = 250;
+        int panelHeight = 230;
         int panelY = centerY - panelHeight / 2;
 
         String[] themes = {"Gamer", "Sea", "Sun", "OLED", "Flash", "Natural", "Rock"};
-        int startY = panelY + 44;
+        int startY = panelY + 36;
 
         for (int i = 0; i < themes.length; i++) {
             String themeName = themes[i];
@@ -47,19 +47,22 @@ public class CrucifiedsThemeScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context);
+
         int centerX = this.width / 2;
         int centerY = this.height / 2;
         int panelWidth = 240;
-        int panelHeight = 250;
+        int panelHeight = 230;
         int panelX = centerX - panelWidth / 2;
         int panelY = centerY - panelHeight / 2;
 
         context.fill(panelX - 1, panelY - 1, panelX + panelWidth + 1, panelY + panelHeight + 1, 0xFF000000);
         context.fillGradient(panelX, panelY, panelX + panelWidth, panelY + panelHeight, CrucifiedTheme.getPrimaryColor(), CrucifiedTheme.getSecondaryColor());
         context.fill(panelX + 1, panelY + 1, panelX + panelWidth - 1, panelY + 2, 0x55FFFFFF);
+
         CrucifiedTheme.renderThemeDecorations(context, panelX, panelY, panelWidth, panelHeight);
 
-        context.drawCenteredTextWithShadow(this.textRenderer, Text.literal("Select Client Theme"), centerX, panelY + 14, 0xFFFFFF);
+        context.drawCenteredTextWithShadow(this.textRenderer, Text.literal("Select Client Theme"), centerX, panelY + 12, 0xFFFFFF);
 
         super.render(context, mouseX, mouseY, delta);
     }
