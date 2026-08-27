a0baa3f399679e8b67da8ed80dbc7991b8149347
idbecrucified <idbecrucified@gmail.com>
2026-08-27 16:44:32 -0600

    Update SurvivalFlyClient.java

--- src/main/java/com/example/survivalfly/SurvivalFlyClient.java
+++ src/main/java/com/example/survivalfly/SurvivalFlyClient.java
@@ -1,11 +1,34 @@
 package com.example.survivalfly;
 
+import com.example.survivalfly.screen.CrucifiedModsScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class SurvivalFlyClient implements ClientModInitializer {
+    public static KeyBinding openMenuKey;
+
     @Override
     public void onInitializeClient() {
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.survivalfly.open_menu",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_SHIFT,
+            "category.survivalfly.title"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openMenuKey.wasPressed()) {
+                if (client.currentScreen == null) {
+                    client.setScreen(new CrucifiedModsScreen(null));
+                }
+            }
+        });
+
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
             HudRenderer.renderHud(drawContext);
         });
